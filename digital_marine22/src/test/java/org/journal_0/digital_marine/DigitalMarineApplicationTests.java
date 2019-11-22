bbe10ecec7fc6a6c63829ab422941624791802f1
pi <pi@host-214-244-171-31.cloudsigma.net>
2019-11-22 17:55:30 +0100

    Initial test

---
+++ digital_marine22/src/test/java/org/journal_0/digital_marine/DigitalMarineApplicationTests.java
@@ -0,0 +1,16 @@
+package org.journal_0.digital_marine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+
+
+
+public class DigitalMarineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
