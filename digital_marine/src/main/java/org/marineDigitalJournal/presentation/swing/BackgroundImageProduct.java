9fe608322bbea05b0328a2eff6e40b86567a2eaa
IsaacDura <pi@host-214-244-171-31.cloudsigma.net>
2020-02-19 13:48:08 +0100

    UpdateCamelCase

---
+++ digital_marine/src/main/java/org/marineDigitalJournal/presentation/swing/BackgroundImageProduct.java
@@ -0,0 +1,43 @@
+package org.marineDigitalJournal.presentation.swing;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+
+public class BackgroundImage_Product extends javax.swing.JPanel {
+
+	// Creates a new form BackgroundImage
+	public BackgroundImage_Product() {
+		
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+		this.setSize(screenSize.width, screenSize.height);
+
+
+	}
+	@Override
+	public void paintComponent(Graphics g) {
+
+		super.paintComponent(g);
+		Dimension size = getSize();
+
+		ImageIcon backgroundImage_Product = new ImageIcon(
+				
+				
+				this.getClass().getResource("results.png"));
+
+		g.drawImage(backgroundImage_Product.getImage(), 0, 0, size.width, size.height, null);
+
+		setOpaque(false);
+		
+		
+		
+		
+	}
+	
+	
+
+
+}
