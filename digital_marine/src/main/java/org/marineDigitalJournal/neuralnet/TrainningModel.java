33d3641c89bd323982b616c1a56276d6c9ab673d
IsaacDura <pi@host-214-244-171-31.cloudsigma.net>
2020-02-19 14:51:11 +0100

    Add Java Doc

--- digital_marine/src/main/java/org/marineDigitalJournal/neuralnet/TrainningModel.java
+++ digital_marine/src/main/java/org/marineDigitalJournal/neuralnet/TrainningModel.java
@@ -1,12 +1,18 @@
 package org.marineDigitalJournal.neuralnet;
-//Activates the iterative model training script
+
+/**
+ * 
+ * Activates the iterative model training script
+ *
+ */
+
 public class TrainningModel extends Thread {
 
 	public void launch() {
 		new Thread() {
 			public void run() {
 				TrainModel trainModel = new TrainModel();
-				trainModel.train_model();
+				trainModel.trainModel();
 
 			}
 		}.start();
